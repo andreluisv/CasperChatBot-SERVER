4fecf50e459c391ccbd04b77abe412ddfeb53910
Andre Luis Vasconcelos <alpvj@cin.ufpe.br>
2021-01-05 17:55:07 -0300

    Responding with news from the DB

--- src/services/chatbot.service.ts
+++ src/services/chatbot.service.ts
@@ -1,85 +1,81 @@
 import Router from 'express';
-import {NewsRepository} from '../repositories/news.repository';
+import { NewsRepository } from '../repositories/news.repository';
 
 const chatbotService = Router()
 const rep = new NewsRepository();
 
-chatbotService.post('/chatbot', async(req, res)=>{
+chatbotService.post('/chatbot', async (req, res) => {
     var theme = req.body.queryResult.intent.displayName || "unknown";
-    theme = (theme==="esportes" ? "Esportes" : "") || 
-    (theme==="politica" ? "Política" : "") || 
-    (theme==="entretenimento" ? "Entretenimento" : "") ||
-    (theme==="famosos" ? "Famosos" : "")  || "unknown";
-    
+    theme = (theme === "esportes" ? "Esportes" : "") ||
+        (theme === "politica" ? "Política" : "") ||
+        (theme === "entretenimento" ? "Entretenimento" : "") ||
+        (theme === "famosos" ? "Famosos" : "") || "unknown";
+
     var newsarr = await rep.findNewsByTheme(theme) || [];
-    const arrsz = newsarr.length || 0;
+    var arrsz = newsarr.length || 0;
     console.log("Chatbot requested for \"" + theme + "\" and found " + arrsz + " news for this theme.");
 
-    if (!arrsz){
+    if (!arrsz) {
         res.json({
             "fulfillmentMessages": [
                 {
-                "text": {
-                    "text": [
-                        "Desculpe-me, mas não encontrei nenhuma notícia sobre esse tema. (TODO -> call for quickreplies)"
-                    ]
-                }
+                    "text": {
+                        "text": [
+                            "Desculpe-me, mas não encontrei nenhuma notícia sobre esse tema. (TODO -> call for quickreplies)"
+                        ]
+                    }
                 }
             ]
         })
         return;
     }
 
-    const card = {
-        "card": {
-          "title": "Quem é a cantora argentina que roubou a cena com Neymar em festa na virada ",
-          "subtitle": " no Brasil deu o que falar. Realizada em Mangaratiba, no Rio de Janeiro, a virada de ano organizada pelo astro contou com a presença de Emilia Mernes, uma cantora argentina que roubou a cena ao dançar com o craque brasileiro na região litorânea conhecida como Costa Verde.",
-          "imageUri": "https://cdn-ofuxico.akamaized.net/img/upload/noticias/2018/03/08/neymar-bruna-marquezine-reproducao-instagram_315593_36.jpg",
-          "buttons": [
-            {
-              "text": "Saiba mais",
-              "postback": "https://www.uol.com.br/esporte/ultimas-noticias/2021/01/05/quem-e-a-cantora-argentina-que-roubou-a-cena-com-neymar-em-festa-na-virada.htm"
-            }
-          ]
-        }
-      };
+    //Bring random news from the collection
+    newsarr = newsarr.sort((a,b) => 0.5 - Math.random());
+    //Limit news to 10
+    arrsz = arrsz>10 ? 10 : arrsz;
 
-    var customElement = {
-    "image_url": "https://upload.wikimedia.org/wikipedia/commons/7/70/Example.png",
-    "default_action": {
-        "webview_height_ratio": "tall",
-        "url": "https://www.google.com/",
-        "type": "web_url"
-    },
-    "buttons": [
-        {
-        "title": "View Website",
-        "type": "web_url",
-        "url": "https://www.google.com/"
-        }
-    ],
-    "title": "Welcome!",
-    "subtitle": "We have the right hat for everyone."
-    };
+    var elementsArray = [];
 
-      var response = {
+    for (let i = 0; i < arrsz; i++){
+        const news = newsarr[i];
+        elementsArray.push({
+            "image_url": news["img_url"]||"",
+            "default_action": {
+                "webview_height_ratio": "tall",
+                "url": news["link"]||"",
+                "type": "web_url"
+            },
+            "buttons": [
+                {
+                    "title": "Saiba mais",
+                    "type": "web_url",
+                    "url": news["link"]||""
+                }
+            ],
+            "title": news["title"]||"",
+            "subtitle": news["description"]||""
+        });
+    }
+
+    var response = {
         "fulfillmentMessages": [
             {
-              "payload": {
-                "facebook": {
-                  "attachment": {
-                    "type": "template",
-                    "payload": {
-                      "elements": [customElement, customElement, customElement],
-                      "template_type": "generic"
+                "payload": {
+                    "facebook": {
+                        "attachment": {
+                            "type": "template",
+                            "payload": {
+                                "elements": elementsArray,
+                                "template_type": "generic"
+                            }
+                        }
                     }
-                  }
-                }
-              },
-              "platform": "FACEBOOK"
+                },
+                "platform": "FACEBOOK"
             }
-          ]
-      }
+        ]
+    }
 
     res.json(response);
 })
